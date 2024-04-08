3a3da30d19c4f19a8566744a152f8bca665b5e83
théoo <90846584+theokbokki@users.noreply.github.com>
2024-04-08 17:55:01 +0200

    Reset homepage and css + add lang

---
+++ lang/en/home.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'pageTitle' => 'Théo Léonets\'s Bookmarks',
+    'title' => 'Bookmarks',
+    'intro' => 'The place where I store and sort all the stuff I find interesting.',
+];
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app title="@lang('home.pageTitle')">
+    <header>
+        <h1>@lang('home.title')</h1>
+        <p>@lang('home.intro')</p>
+    </header>
+    <main></main>
+</x-layouts.app>
