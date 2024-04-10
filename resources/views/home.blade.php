ef8f5f95c7cb51443bd291044c81323b99543804
théoo <90846584+theokbokki@users.noreply.github.com>
2024-04-10 21:50:36 +0200

    Add filters styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,31 +4,57 @@
         <p class="hero__subtitle">{{ __('home.intro') }}</p>
     </header>
     <main class="main content">
-        <div>
-           <form action="{{ request('url') }}">
-                <fieldset>
-                    <label for="search">{{ __('home.filters.search') }}</label>
+        <div class="filters">
+            <form action="{{ request('url') }}"
+                class="filters__form"
+            >
+                <fieldset class="filters__item">
+                    <label for="search"
+                        class="filters__label"
+                    >
+                        {{ __('home.filters.search') }}
+                    </label>
                     <input type="search"
                         name="search"
                         id="search"
                         placeholder="{{ __('home.filters.searchEmpty') }}"
+                        class="filters__input"
                     >
                 </fieldset>
-                <fieldset>
-                <legend>{{ __('home.filters.tagEmpty') }}</legend>
-                @foreach($tags as $tag)
-                    <div>
-                        <input
-                            type="checkbox"
-                            id="{{ $tag->slug }}"
-                            name="tags[]"
-                            value="{{ $tag->slug }}"
-                        >
-                        <label for="{{ $tag->slug }}">{{ $tag->name }}</label>
+                <fieldset class="filters__item">
+                    <input type="checkbox"
+                        class="filters__toggle sro"
+                        name="checkbox-toggle"
+                        id="checkbox-toggle"
+                    >
+                    <p>Tags</p>
+                    <label for="checkbox-toggle"
+                        class="filters__input"
+                    >
+                        {{ __('home.filters.tagEmpty') }}
+                    </label>
+                    <div class="filters__dropdown dropdown">
+                        @foreach($tags as $tag)
+                            <div class="dropdown__item">
+                                <input
+                                    type="checkbox"
+                                    id="{{ $tag->slug }}"
+                                    name="tags[]"
+                                    value="{{ $tag->slug }}"
+                                    class="dropdown__input sro"
+                                >
+                                <label for="{{ $tag->slug }}"
+                                    class="dropdown__label"
+                                >{{ $tag->name }}</label>
+                            </div>
+                        @endforeach
                     </div>
-                @endforeach
                 </fieldset>
-                <button type="submit">{{ __('home.filters.submit')}}</button>
+                <button type="submit"
+                    class="filters__submit"
+                >
+                    {{ __('home.filters.submit')}}
+                </button>
            </form>
         </div>
         <div>
