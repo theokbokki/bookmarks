1163e867e523d3156022d49eb2b2724e64394229
théoo <90846584+theokbokki@users.noreply.github.com>
2024-09-26 07:54:24 +0200

    Add laravel folio

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,20 @@
+<?php
+
+use function Laravel\Folio\name;
+
+name('home');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookmarks</title>
+</head>
+<body>
+    <h1>Bookmarks</h1>
+</body>
+</html>
